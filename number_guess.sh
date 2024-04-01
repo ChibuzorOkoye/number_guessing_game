f16647bafa6a5c960c727344cf35f5ec06c8dbad
Chibuzor Okoye <Chibuzorokoye77@gmail.com>
2024-04-01 14:30:49 +0000

    feat: the script connects to the database properly

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo -e "\n~~~Number Guessing Game ~~~\n"
 
@@ -8,19 +8,20 @@ echo Enter your username:
 read USERNAME
 
 RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
-GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
-BEST_GAME=$($PSQL "SELECT MIN(num_of_guesses) FROM games WHERE user_id = $USER_ID")
+
 if [[ -z $RETURNING_USER ]]
   then
   INSERT_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
     echo Welcome, $USERNAME! It looks like this is your first time here.
 
   else
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
+BEST_GAME=$($PSQL "SELECT MIN(num_of_guesses) FROM games WHERE user_id = $USER_ID")
   echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 
-RANDOM_NUM=$((1 + $RANDOM%1000))
+RANDOM_NUM=$((1 + $RANDOM % 1000))
 echo "Guess the secret number between 1 and 1000:"
 
 while read GUESS
