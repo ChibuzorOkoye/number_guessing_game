3d92205753447e3dd65422a7a24dc117f6e5e896
Chibuzor Okoye <Chibuzorokoye77@gmail.com>
2024-04-01 14:14:56 +0000

    fix: added outline of code

--- number_guess.sh
+++ number_guess.sh
@@ -7,21 +7,44 @@ echo -e "\n~~~Number Guessing Game ~~~\n"
 echo Enter your username:
 read USERNAME
 
-RETURNING_USER=$($PSQL SELECT username FROM users WHERE username = $USERNAME)
-GAMES_PLAYED=$($PSQL SELECT games_played FROM games WHERE player_name = $RETURNING_USER)
-BEST_GAME=$($PSQL SELECT best_game FROM games WHERE player_name = $RETURNING_USER)
+RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
+BEST_GAME=$($PSQL "SELECT MIN(num_of_guesses) FROM games WHERE user_id = $USER_ID")
 if [[ -z $RETURNING_USER ]]
   then
+  INSERT_NEW_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
     echo Welcome, $USERNAME! It looks like this is your first time here.
 
   else
-  echo Welcome back, $RETURNING_USER! You played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
+  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 
-echo "Guess the secret number between 1 and 1000"
-read GUESS
-
 RANDOM_NUM=$((1 + $RANDOM%1000))
+echo "Guess the secret number between 1 and 1000:"
+
+while read GUESS
+do
+  if [[ $GUESS -lt $RANDOM_NUM ]]
+    then
+      echo "It's higher than that, guess again:"
+  if [[ $GUESS -gt $RANDOM_NUM ]]
+  then
+      echo "It's lower than that, guess again:"
+  if [[ $GUESS -eq $RANDOM_NUM ]]
+  then
+      echo You guessed it in $COUNT tries. The secret number was $RANDOM_NUM. Nice job!
+      exit
+
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+    then
+    echo "That is not an integer, guess again:"
+    fi
+    fi
+    fi
+  fi
+  COUNT=$(( $COUNT + 1 ))
+  done
 
 
 
@@ -30,6 +53,4 @@ RANDOM_NUM=$((1 + $RANDOM%1000))
 
 
 
-
-
-#USER_INFO=$($PSQL INSERT INTO number_guess (name,))
+USER_INFO=$($PSQL "INSERT INTO games (num_of_guesses,user_id)  VALUES($COUNT, $USER_ID))"
